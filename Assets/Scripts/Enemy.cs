b16c9c4fb826a212b4a78e59717dae57ea6e2bb7
MizAless <44200635+MizAless@users.noreply.github.com>
2024-06-03 23:01:59 +0700

    Small fixes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,14 +4,14 @@ public abstract class Enemy : MonoBehaviour
 {
     protected IMover Mover;
 
-    public void Init(Target target)
-    {
-        Mover.SetTarget(target);
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent<Target>(out _))
             Destroy(gameObject);
+    }
+
+    public void Init(Target target)
+    {
+        Mover.SetTarget(target);
     }
 }
